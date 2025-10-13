3322f60d71c089af9378fb99a454a5b5c5ebcb9c
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-10-13 17:22:40 +0500

    conn and add to db

---
+++ db/db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type DBshort struct {
+	DB *sql.DB
+}
+
+func Conn() (*DBshort, error) {
+	connStr := "user=ilya password=123 dbname=shortener host=localhost port=5432 sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &DBshort{DB: db}, nil
+}
+
+func (d *DBshort) Add(fullUrl, shortUrl string) error {
+	query := "INSERT INTO urls (FullURL, ShortURL) VALUES ($1, $2)"
+	_, err := d.DB.Exec(query, fullUrl, shortUrl)
+	if err != nil {
+		return err
+	}
+	return nil
+}
