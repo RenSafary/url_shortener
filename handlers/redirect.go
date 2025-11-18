7579d18e271e6dcd0eac59d64cfbf2c4fe7ee599
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-11-18 11:16:35 +0500

    redirect with db

--- handlers/redirect.go
+++ handlers/redirect.go
@@ -1,14 +1,27 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"URL_Shortener/db"
+
+	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
 func Redirect(d *db.DBshort) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		short := vars["link"]
 
+		full, err := d.Get(short)
+		if err != nil {
+			log.Println("Couldn't get full link:", err)
+			http.Redirect(w, r, "/error", http.StatusSeeOther)
+			return
+		}
+
+		http.Redirect(w, r, full, http.StatusMovedPermanently)
 	}
 }
